3ee9454b8af4d7ba3ef704dff2c15ff017661447
Souleymane Mayana Mamane Bachir <61478792+MecatroBachir@users.noreply.github.com>
2020-03-19 19:03:27 +0100

    Update Chase.ino
    This code switchs ON all the leds connected to the Arduino one by one with 0.5 seconds time interval. It's usually called chase.

--- Chase.ino
+++ Chase.ino
@@ -1,41 +1,17 @@
-
+int i = 0 ;
 void setup() {          // Defining mode : output or input
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(7, OUTPUT);
-  pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
-
+  for (i=2 ; i<=10 ; i++)
+      pinMode(i, OUTPUT);
 }
 
 void loop() {               // Switchin OFF all the leds
   delay(2000);
-  digitalWrite(2, LOW);
-  digitalWrite(3, LOW);
-  digitalWrite(5, LOW);
-  digitalWrite(7, LOW);
-  digitalWrite(8, LOW);
-  digitalWrite(9, LOW);
-  digitalWrite(10, LOW);
-  digitalWrite(11, LOW);
+  for (i=2 ; i<=10 ; i++)
+    digitalWrite(i, LOW);
                           // Switchin ON all the leds on by one with 0.5 seconds time interval
-  digitalWrite(2, HIGH);
-  delay(500);
-  digitalWrite(3, HIGH);
-  delay(500);
-  digitalWrite(5, HIGH);
-  delay(500);
-  digitalWrite(7, HIGH);
-  delay(500);
-  digitalWrite(8, HIGH);
-  delay(500);
-  digitalWrite(9, HIGH);
-  delay(500);
-  digitalWrite(10, HIGH);
-  delay(500);
-  digitalWrite(11, HIGH);
-  delay(500);
+  for (i=2 ; i<=10 ; i++)
+  {
+      digitalWrite(i, HIGH);
+      delay(500);
+  }
 }
