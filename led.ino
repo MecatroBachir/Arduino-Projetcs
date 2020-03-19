29a6b3bffc87469bfe3590caa23d9eabf203e009
Souleymane Mayana Mamane Bachir <61478792+MecatroBachir@users.noreply.github.com>
2020-03-19 18:30:40 +0100

    Update led.ino
    This code will turn on your led each one second.

--- led.ino
+++ led.ino
@@ -1,11 +1,12 @@
-void setup() {
-  pinMode(2, OUTPUT);
-
+int pinLed = 2 ;  // Defining the pin of the led
+void setup() { // Running one time
+  pinMode(pinLed, OUTPUT);     // Defining the mode of the pin : output or input
+ 
 }
 
-void loop() {
-  digitalWrite(2, LOW);
-  delay(1000);
-  digitalWrite(2, HIGH);
-  delay(1000);
+void loop() {    // Void loop
+  digitalWrite(pinLed, LOW);     // Turning off the led
+  delay(1000);        // setting 1 seconde delay
+  digitalWrite(pinLed, HIGH);       // Turning on the led
+  delay(1000); 
 } 
