b613c1a8f879cf9d9ec024b7f3e537d2d9deb40e
Souleymane Mayana Mamane Bachir <61478792+MecatroBachir@users.noreply.github.com>
2020-03-27 03:14:41 +0100

    Update Chase.ino

--- Chase.ino
+++ Chase.ino
@@ -1,15 +1,15 @@
 int i = 0 ;
 void setup() {          // Defining mode : output or input
-  for (i=2 ; i<=10 ; i++)
+  for (i=3 ; i<=10 ; i++)
       pinMode(i, OUTPUT);
 }
 
 void loop() {               // Switchin OFF all the leds
   delay(2000);
-  for (i=2 ; i<=10 ; i++)
+  for (i=3 ; i<=10 ; i++)
     digitalWrite(i, LOW);
                           // Switchin ON all the leds on by one with 0.5 seconds time interval
-  for (i=2 ; i<=10 ; i++)
+  for (i=3 ; i<=10 ; i++)
   {
       digitalWrite(i, HIGH);
       delay(500);
